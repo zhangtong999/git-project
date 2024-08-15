ba39fab62beb17c00624d2e82acb2374c7c03094
itcast <hello@itcast.cn>
2024-08-15 17:16:35 +0800

    三次提交

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("htllo git");
         System.out.println("htllo git2");
+        System.out.println("htllo git3");
     }
 }
