9bfe48585a759b72c7f6de64dec284d65edbe997
itcast <hello@itcast.cn>
2024-08-15 22:20:59 +0800

    Revert "revert test 二次二次滚滚滚"
    This reverts commit ffd7c1da7396a1ed8b54870949ad7bbc46b70a87.

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -15,7 +15,5 @@ public class GitTest {
         System.out.println("htllo pullllll git test2!!");
 
         System.out.println("htllo pullllll git 回滚滚滚滚");
-
-        System.out.println("htllo pullllll git 二次二次滚滚滚");
     }
 }
