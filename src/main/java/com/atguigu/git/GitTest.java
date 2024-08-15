1d7cba5babb1af04bd759b200e9fb33286777f75
itcast <hello@itcast.cn>
2024-08-15 17:50:37 +0800

    hotfix 提交2

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("htllo git2");
         System.out.println("htllo git3");
         System.out.println("htllo git hot fix");
+        System.out.println("htllo git hot fix22");
     }
 }
